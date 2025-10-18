21909573009e0aca08f4e2be60823622a39e0b85
Muhammad Zeeshan <mzeeshan@godats.com>
2025-10-18 15:35:28 +0500

    Order Placed Successfully

--- src/api/coupon.js
+++ src/api/coupon.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
-// const BASE_URL = import.meta.env.VITE_BASE_URL || "https://crunchy-cookies-server.onrender.com/api/v1"
-const BASE_URL = "http://localhost:5000/api/v1"
+const BASE_URL = import.meta.env.VITE_BASE_URL || "https://crunchy-cookies-server.onrender.com/api/v1"
+// const BASE_URL = "http://localhost:5000/api/v1"
 
 export const checkCoupon = async (payload) => {
     try {
