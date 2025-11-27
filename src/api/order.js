2097f664d3846b3fee64e8f6d87304909dabd8a4
Muhammad Shayan <shayanarif666@gmail.com>
2025-11-27 15:25:55 +0500

    Payment Reciept and Transaction record

--- src/api/order.js
+++ src/api/order.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:5000/api/v1"
+const BASE_URL = import.meta.env.VITE_BASE_URL || "https://crunchy-cookies-server.onrender.com/api/v1"
 // const BASE_URL = "http://localhost:5000/api/v1"
 
 export const getOnGoingOrderByUser = async (userId) => {
--- src/api/payments.js
+++ src/api/payments.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const BASE_URL =
-  import.meta.env.VITE_BASE_URL || "http://localhost:5000/api/v1";
+  import.meta.env.VITE_BASE_URL || "https://crunchy-cookies-server.onrender.com/api/v1";
 
 export const getPayments = async () => {
   try {
